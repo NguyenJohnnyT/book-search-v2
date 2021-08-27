104b004add32fadcdcbce18cd94d29dc316a075c
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2021-08-26 22:41:07 -0700

    configure server for apollo v3

--- server/server.js
+++ server/server.js
@@ -10,13 +10,27 @@ const { ApolloServer } = require('apollo-server-express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 //|NEW INSTANCE OF APOLLO SERVER|
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
+async function startServer () {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
 
-//| APPLY APP MIDDLEWARE |
-server.applyMiddleware({ app });
+  await server.start();
+  //| APPLY APP MIDDLEWARE |
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
+    });
+  });
+}
+
+startServer()
+
+// server.applyMiddleware({ app });
 
 //| MIDDLEWARE |
 app.use(express.urlencoded({ extended: true }));
@@ -28,16 +42,10 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 //| WILDCARE ROUTE FOR SERVING FRONT END |
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
 //| NO ROUTES NEEDED FOR MERN |
 // app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
-  });
-});
