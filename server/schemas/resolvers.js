90cfc1e5ab43ac81ff4c1094c28aa2e1c41c09d7
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2021-08-26 23:05:06 -0700

    login/signup mutation routes

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -18,6 +18,31 @@ const resolvers = {
       }
       throw new AuthenticationError('You must be logged in!');
     }
+  },
+
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return {token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('No user found with this email address');
+      }
+
+      const validatePw = await user.isCorrectPassword(password);
+
+      if (!validatePw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    }
   }
 }
 
