0e7dcede73c5a6b74a510da7d9fcb051fe202714
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2021-08-26 22:10:31 -0700

    apollo express code

--- server/server.js
+++ server/server.js
@@ -1,11 +1,24 @@
+//|EXPRESS|
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+// const routes = require('./routes');
+//| APOLLO |
+const { typeDefs, resolvers } = require('./schemas');
+const { ApolloServer } = require('apollo-server-express');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+//|NEW INSTANCE OF APOLLO SERVER|
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
+//| APPLY APP MIDDLEWARE |
+server.applyMiddleware({ app });
+
+//| MIDDLEWARE |
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -14,8 +27,17 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+//| WILDCARE ROUTE FOR SERVING FRONT END |
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+//| NO ROUTES NEEDED FOR MERN |
+// app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
+  });
 });
