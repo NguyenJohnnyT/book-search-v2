71ef4bbb4eeb64c540e4962f6870a247b2bdc9f1
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2021-08-27 20:04:07 -0700

    QUERY_SINGLE_USER

--- client/src/utils/MERN/queries.js
+++ client/src/utils/MERN/queries.js
@@ -10,4 +10,21 @@ export const QUERY_USERS = gql`
       savedBooks
     }
   }
-  `;
+`;
+
+export const QUERY_SINGLE_USER = gql`
+  query singleUser($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      savedBooks {
+        bookId
+        title
+        authors
+        description
+        image
+        link
+      }
+    }
+  }
+`;
