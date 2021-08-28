0d8f25c63fdd02f1d6a4fd360aceb75bafcd50c4
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2021-08-27 20:08:34 -0700

    add query_me, set up mutations.js

--- client/src/utils/MERN/mutations.js
+++ client/src/utils/MERN/mutations.js
@@ -1,1 +1,17 @@
 const { gql } = require('apollo-server-express');
+
+export const LOGIN_USER = gql`
+
+`;
+
+export const ADD_USER = gql`
+
+`;
+
+export const SAVE_BOOK = gql`
+
+`;
+
+export const REMOVE_BOOK = gql`
+
+`;
--- client/src/utils/MERN/queries.js
+++ client/src/utils/MERN/queries.js
@@ -28,3 +28,13 @@ export const QUERY_SINGLE_USER = gql`
     }
   }
 `;
+
+export const QUERY_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+    }
+  }
+`;
