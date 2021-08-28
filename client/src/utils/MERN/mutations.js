4a650f3d5696b343d944f0a9ae1b6bf141e96af4
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2021-08-27 20:10:11 -0700

    login_user code

--- client/src/utils/MERN/mutations.js
+++ client/src/utils/MERN/mutations.js
@@ -1,7 +1,15 @@
 const { gql } = require('apollo-server-express');
 
 export const LOGIN_USER = gql`
-
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      profile {
+        _id
+        username
+      }
+    }
+  }
 `;
 
 export const ADD_USER = gql`
