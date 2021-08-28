1b4eb62308838e86fb95ee2ea7fa70ebd77151ef
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2021-08-27 21:26:47 -0700

    useMutation and hook signup

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { createUser } from '../utils/API';
+import { useMutation } from '@apollo/client'; //!APOLLO SIGNUP
+import { ADD_USER } from '../utils/MERN/mutations';
+
+// import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
@@ -11,6 +14,7 @@ const SignupForm = () => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+  const [ signup, { error, data }] = useMutation(ADD_USER)
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -28,7 +32,12 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      // const response = await createUser(userFormData);
+      const { data } = await signup({
+        variables: { ...userFormData },
+      });
+
+      Auth.login(data.signup.token)
 
       if (!response.ok) {
         throw new Error('something went wrong!');
